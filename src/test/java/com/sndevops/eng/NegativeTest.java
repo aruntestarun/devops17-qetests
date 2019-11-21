8b5a6bdbc034a9e4b72556cfba461797880b8a15
aruntestarun <45982564+aruntestarun@users.noreply.github.com>
2019-11-21 14:58:56 -0800

    Paul: test payload

--- src/test/java/com/sndevops/eng/NegativeTest.java
+++ src/test/java/com/sndevops/eng/NegativeTest.java
@@ -7,11 +7,11 @@ import org.junit.Ignore;
 
 public class NegativeTest {
     private String INPUT = "4";
-    @Ignore
+
     @Test
     public void negativeTest1() throws Exception{
         Thread.sleep(5000);
-        Assert.assertEquals(INPUT,"44");
+        Assert.assertEquals(INPUT,"4");
         
     }
 
